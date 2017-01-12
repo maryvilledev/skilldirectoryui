d460d866503c7c4083c46aef4d3ed8665f87a4eb
Hopding <andrew.dillon.j@gmail.com>
2017-01-12 12:33:09 -0600

    Updated Skills component unit test

--- src/Skills.test.jsx
+++ src/Skills.test.jsx
@@ -15,10 +15,41 @@ describe('<Skills />', () => {
       expect(wrapper.get(0)).toMatchSnapshot();
     });
 
-    it('has a Button that opens the Modal', () => {
+    it('has a Button that opens the SkillModal', () => {
+      // Setup
       const wrapper = mount(<Skills />);
-      expect(wrapper.find('Modal').prop('isOpen')).toBe(false);
-      wrapper.find('Button').simulate('click');
-      expect(wrapper.find('Modal').prop('isOpen')).toBe(true);
+      const skillModalNode = wrapper.find('Modal').
+                findWhere(n => n.debug().includes('contentLabel="SkillModal"'));
+      const addSkillNode = wrapper.find('button').
+                findWhere(n => n.debug().includes('name="add_skill"'));
+
+      // Run Test
+      expect(skillModalNode.prop('isOpen')).toBe(false);
+      addSkillNode.simulate('click');
+      expect(skillModalNode.prop('isOpen')).toBe(true);
+    });
+
+    it('has an "Add Link" button that is disabled until a Skill is selected', () => {
+      const wrapper = mount(<Skills />);
+      const addLinkNode = wrapper.find('button').
+                 findWhere(n => n.debug().includes('name="add_link"'));
+      expect(addLinkNode.props().disabled).toBe(true);
+      wrapper.setState({ currentSkill: { skill_id: "1234" }});
+      expect(addLinkNode.props().disabled).toBe(false);
+    });
+
+    it('has a Button that opens the LinkModal', () => {
+      // Setup
+      const wrapper = mount(<Skills />);
+      wrapper.setState({ currentSkill: { skill_id: "1234" }});
+      const linkModalNode = wrapper.find('Modal').
+                 findWhere(n => n.debug().includes('contentLabel="LinkModal"'));
+      const addLinkNode = wrapper.find('button').
+                 findWhere(n => n.debug().includes('name="add_link"'));
+
+      // Run Test
+      expect(linkModalNode.prop('isOpen')).toBe(false);
+      addLinkNode.simulate('click');
+      expect(linkModalNode.prop('isOpen')).toBe(true);
     });
 });
