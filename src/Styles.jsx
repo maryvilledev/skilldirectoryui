3f6ced4da6af62884854ce6666bf83e43721dee8
Jordan Davis <Jordan.davis@maryville.com>
2017-02-16 11:42:21 -0600

    merge master

--- src/Styles.jsx
+++ src/Styles.jsx
@@ -1,10 +1,16 @@
 export const ModalStyle = {
   overlay: {
-    position: 'fixed',
-    top: '15%',
-    left: '25%',
-    right: '25%',
-    bottom: '25%',
+    position: 'absolute',
+    top: '0%',
+    left: '0%',
+    right: '0%',
+    bottom: '0%',
   },
-  content: {}
+  content: {
+    position: 'fixed',
+    top: '250px',
+    left: '500px',
+    right: '500px',
+    bottom: '250px',
+  }
 }
